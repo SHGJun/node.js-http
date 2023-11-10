f4a3593b25af18fd0153bec03124c95143f0c38d
时光SHG <17777552458@163.com>
2023-11-10 16:02:16 +0800

    app.js代码优化

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-//实现登录注册功能
-//导入模块
 const http = require('http')
-//第三方模块 uuid 生成id的 生成一个32位的id（不重复的）
-// const { v4 } = require('uuid')
-// const { resolve } = require('path')
-//导入加密的模块 md5 (hash加密法 被破解 加盐（为了安全）)
-// const md5 = require('md5')
 const routePathnameRequest = require('./utils');
 
-// 设置相应Header
-const setHeaderInfo = (res) => {
-    //设置响应头
-    res.setHeader('content-type', 'text/json;charset=utf-8')
-    //解决跨域问题
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Origin-Method', '*')
-}
+const port = '9999';
+const domainName = '127.0.0.1';
 
 //创建服务
 http.createServer((req, res) => {
-    setHeaderInfo(res);
+    res.setHeader('content-type', 'text/json;charset=utf-8')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin-Method', '*')
     routePathnameRequest(req,res);
-}).listen('9999', '127.0.0.1', () => {
-    console.log('server is running,at 127.0.0.1:9999')
+}).listen(port, domainName, () => {
+    console.log(`服务器启动成功，可访问 http://${domainName}:${port}`);
 })
